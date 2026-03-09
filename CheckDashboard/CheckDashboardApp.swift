e4e15d2caefd3204a7e39dcb7987defeafb349fc
Bogdan Tudosie <bogdan.tudosie@withsecure.com>
2026-03-09 13:43:46 +0200

    Initial Commit

---
+++ CheckDashboard/CheckDashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckDashboardApp.swift
+//  CheckDashboard
+//
+//  Created by Bogdan Tudosie on 9.3.2026.
+//
+
+import SwiftUI
+
+@main
+struct CheckDashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
